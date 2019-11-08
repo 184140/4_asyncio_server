16d1496ca625e0565e16c1e7700267b2bfbca208
184140 <56712629+184140@users.noreply.github.com>
2019-11-08 23:02:08 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,32 @@
 import asyncio
 
-HOST = 'localhost'
-PORT = 9095
-
 
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
+    data = await reader.read(1024)
     message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(f"Received {message} from {addr}")
 
     writer.write(data)
     await writer.drain()
+    print(f"{data} sent back")
 
     writer.close()
+    print("Client socket closed")
 
 
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
+coro = asyncio.start_server(handle_echo, '127.0.0.1', 9090, loop=loop)
 server = loop.run_until_complete(coro)
 
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
+# работает, пока не нажмём Ctrl+C
+print(f'Server based on {server.sockets[0].getsockname()}')
 try:
     loop.run_forever()
 except KeyboardInterrupt:
     pass
 
-# Close the server
+# закрываем сервер
 server.close()
 loop.run_until_complete(server.wait_closed())
 loop.close()
